241d328b420769db901e0cbdab875f5a77a3521a
kiyo <kiyonari6318@yahoo.co.jp>
2021-02-08 06:58:35 +0900

    Add: 記事詳細ページ

--- components/Post.js
+++ components/Post.js
@@ -1,13 +1,31 @@
-const Post = ({ post }) => {
+import Link from "next/link";
+
+// const Post = ({ post }) => {
+//   return (
+//     <div>
+//       <span>{post.id}</span>
+//       {" : "}
+//       <Link href={`/posts/${post.id}`}>
+//         <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+//           {post.title}
+//         </span>{" "}
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default Post;
+
+export default function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
       {" : "}
-      <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
-        {post.title}
-      </span>
+      <Link href={`/posts/${post.id}`}>
+        <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+          {post.title}
+        </span>
+      </Link>
     </div>
   );
-};
-
-export default Post;
+}
